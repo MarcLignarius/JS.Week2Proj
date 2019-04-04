e081dc1e4f5e58b957b4abe537548185bfa98355
Marc Davies <marcdaviesriot@gmail.com>
2019-04-04 00:38:41 -0700

    remove addClass/removeClass and add .show()/.hide(). Hide function still not working when selecting new path

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,26 @@
+var userName= $("input#name").val();
+$(".username").text(userName);
+
 $(function() {
-  $("#survey").submit(function(event){
+  $("form#survey").submit(function(event){
 		var city = parseInt($("input:radio[name=city]:checked").val());
-    var companysize = parseInt($("input:radio[name=companysize]:checked").val());
+    var companySize = parseInt($("input:radio[name=companysize]:checked").val());
 		var team = parseInt($("input:radio[name=team]:checked").val());
     var project = parseInt($("input:radio[name=project]:checked").val());
-    var topreason = parseInt($("input:radio[name=topreason]:checked").val());
-    if (city === 1 && companysize === 1 && team === 1 && project === 1 && topreason === 1) {
-      $("#result1").removeClass();
-      $("#result2, #result3").addClass("hide");
-      $("#result1").toggle();
-    } else if (city === 2 && companysize === 2 && team === 2 && project === 2 && topreason === 2) {
-        $("#result2").removeClass();
-        $("#result1, #result3").addClass("hide");
-        $("#result2").toggle();
-          } else if (city === 3 && companysize === 3 && team === 3 && project === 3 && topreason === 3) {
-            $("#result3").removeClass();
-            $("#result1, #result3").addClass("hide");
-            $("#result3").toggle();
-      };
-      event.preventDefault();
-      var username= $("input#name").val();
-      $(".username").text(username);
-    });
+    var topReason = parseInt($("input:radio[name=topreason]:checked").val());
+    if (city === 1 && companySize === 1 && team === 1 && project === 1 && topReason === 1) {
+      $("#noresult", "#result2", "#result3").hide();
+      $("#result1").show();
+    } else if (city === 2 && companySize === 2 && team === 2 && project === 2 && topReason === 2) {
+      $("#noresult", "#result1", "#result3").hide();
+      $("#result2").show();
+    } else if (city === 3 && companySize === 3 && team === 3 && project === 3 && topReason === 3) {
+      $("#noresult", "#result1", "#result2").hide();
+      $("#result3").show();
+    } else {
+      $("#result1", "#result2", "#result3").hide();
+      $("#noresult").show();
+    }
+    event.preventDefault();
+  });
 });
